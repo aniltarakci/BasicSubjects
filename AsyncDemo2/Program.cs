35e4a625bb6e40babf0d55889cb2fad70338e991
ANIL <ANIL@DESKTOP-LQCPFUV>
2022-09-20 23:08:45 +0300

    Add project files.

---
+++ AsyncDemo2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread: {Thread.CurrentThread.ManagedThreadId}");
+            Process1();
+            Process1Async();
+            Process2();
+            Process2Async();
+        }
+
+        static void Process1()
+        {
+            Console.WriteLine($"Senkron işlem 1 başladı. {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process1Async()
+        {
+            await Task.Run(() =>
+             {
+                 Console.WriteLine($"Asenkron işlem 1 başladı. {Thread.CurrentThread.ManagedThreadId}");
+             });
+        }
+
+        static void Process2()
+        {
+            Console.WriteLine($"Senkron işlem 2 başladı. {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process2Async()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Asenkron işlem 2 başladı. {Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+    }
+}
